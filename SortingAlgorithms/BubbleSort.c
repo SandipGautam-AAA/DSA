5965faeb92b089f425b5ff06c893568e5f256126
Sandip Gautam <Sandeepcgautam@gmail.com>
2026-01-28 15:21:33 +0545

    Optimize the Bubble sort Algorith

--- SortingAlgorithms/BubbleSort.c
+++ SortingAlgorithms/BubbleSort.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+#include<stdbool.h>
 
+// Time Complexity -> O(n^2)   Best case-> O(n)
+// Space Complexity -> O(1)
+// In every ith round ith largest/smalest came to its place -> sorting is done by back of the array
+
+// Flag isSwap is used to Optamize the program if the array is sorted(noSwap) before the i = n-1th round  SS
 int main() {
     int arr[10] = {2, 40, 34, 13, 43, 42, 12, 22, 32, 46};
     
@@ -7,14 +13,20 @@ int main() {
     // printf("%d", n);
 
     for(int i = 0; i < (n - 1); i++ ) {
-        for(int j = 0; j < (n - i - 1); j++) {
 
+        bool isSwap = false;
+        for(int j = 0; j < (n - i - 1); j++) {
             if(arr[j] > arr[j + 1]){
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp; 
+
+                isSwap = true;
             }
         }
+        if(isSwap == false) // already sorted
+            break;
+        
     }
 
     for(int i = 0; i<n; i++){
